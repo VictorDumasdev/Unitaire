402f35b1179727098bdc659b7b8fde4fa10df519
Victor <dumas.victor97@gmail.com>
2021-03-15 14:54:14 +0100

    Revert "Revert "testAdherentNomPrenomAlpha""
    This reverts commit 617622cc6a34833bef2ac9651e305ed8857d5127.

--- src/Search/AdherentSearch.php
+++ src/Search/AdherentSearch.php
@@ -8,10 +8,13 @@ class AdherentSearch
 {
     public function identifiantNormalise(Adherent $adherent) {
         $nom = $adherent->getNom();
+        $nom = preg_replace('^[a-zA-Z]+$', '', $nom);
         $prenom = $adherent->getPrenom();
+        $prenom = preg_replace('^[a-zA-Z]+$', '', $prenom);
         $date = $adherent->getDateNaissance();
         $ident = $date->format("dmY") . strtoupper($nom) . '_' . strtoupper($prenom);
         $ident = str_replace('-','',$ident);
+
         return $ident;
     }
 }
